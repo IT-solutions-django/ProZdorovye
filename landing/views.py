4102b3727329256edea675b0d0508b967eb0d05d
Пак Сергей <sergegnabri2005@gmail.com>
2024-10-09 21:31:55 +0700

    Add home page design (hard-code)

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class HomeView(View): 
+    template_name = 'landing/main.html' 
+
+    def get(self, request): 
+        return render(request, self.template_name)
