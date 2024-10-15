59960d6596bb87d5fec6b510cf02343aa38bd042
Пак Сергей <sergegnabri2005@gmail.com>
2024-10-15 16:11:25 +0700

    Change speciality image from many-to-many field to foreign key from another table

--- services/models.py
+++ services/models.py
@@ -6,8 +6,29 @@ import os
 from django.urls import reverse
 
 
+class Speciality(models.Model): 
+    name = models.CharField('Название', max_length=80)
+    intro = models.CharField('Очень короткое описание', max_length=150)
+    short_description = models.CharField('Короткое описание', max_length=500)
+    description = models.TextField('Описание')
+    icon = models.FileField('Иконка', upload_to='specialities', null=True)
+    slug = models.SlugField('Слаг')
+    
+    class Meta: 
+        ordering = ['name']
+        verbose_name = 'Специализация'
+        verbose_name_plural = 'Специализации'
+
+    def __str__(self) -> str: 
+        return self.name
+
+    def get_absolute_url(self): 
+        return reverse('services:speciality', args=[self.slug])
+    
+
 class SpecialityPhoto(models.Model):
     image = models.ImageField('Изображение', upload_to='articles', null=True, blank=True)
+    speciality = models.ForeignKey(verbose_name='Специализация', to=Speciality, on_delete=models.CASCADE, related_name='photos', null=True)
     created_at = models.DateTimeField('Дата загрузки', auto_now_add=True)
 
     class Meta:
@@ -32,27 +53,6 @@ class SpecialityPhoto(models.Model):
         )
         self.image.save(f"{name}.webp", img_file, save=False)
         super(SpecialityPhoto, self).save(*args, **kwargs)
-
-
-class Speciality(models.Model): 
-    name = models.CharField('Название', max_length=80)
-    intro = models.CharField('Очень короткое описание', max_length=150)
-    short_description = models.CharField('Короткое описание', max_length=500)
-    description = models.TextField('Описание')
-    icon = models.FileField('Иконка', upload_to='specialities', null=True)
-    photos = models.ManyToManyField(SpecialityPhoto, verbose_name='Фотографии', blank=True)
-    slug = models.SlugField('Слаг')
-    
-    class Meta: 
-        ordering = ['name']
-        verbose_name = 'Специализация'
-        verbose_name_plural = 'Специализации'
-
-    def __str__(self) -> str: 
-        return self.name
-
-    def get_absolute_url(self): 
-        return reverse('services:speciality', args=[self.slug])
     
 
 class Symptom(models.Model): 
