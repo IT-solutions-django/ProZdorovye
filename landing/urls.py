7ed56bf4667e022a744751b8aefcda8d0a43f475
Пак Сергей <sergegnabri2005@gmail.com>
2024-10-20 17:07:20 +0700

    Fix URL of the homepage: from home/ to /

--- landing/urls.py
+++ landing/urls.py
@@ -6,7 +6,7 @@ app_name = 'landing'
 
 
 urlpatterns = [
-    path('home/', HomeView.as_view(), name='home'), 
+    path('', HomeView.as_view(), name='home'), 
     path('contacts/', ContactsView.as_view(), name='contacts'), 
     path('request_saved/', RequestSavedView.as_view(), name='request_saved'),
 
