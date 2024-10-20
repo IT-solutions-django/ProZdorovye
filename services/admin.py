beb55af96c8d4cb7f9fe96e6a3b9afd38ad6ac84
Пак Сергей <sergegnabri2005@gmail.com>
2024-10-20 17:35:02 +0700

    Update admin panel

--- services/admin.py
+++ services/admin.py
@@ -10,16 +10,14 @@ class SpecialityPhotoInline(admin.TabularInline):
     model = SpecialityPhoto
     extra = 0
 
+class ProfessionInline(admin.TabularInline): 
+    model = Profession
+    extra = 0
+
 
 @admin.register(Speciality)
 class SpecialityAdmin(admin.ModelAdmin): 
     list_display = ['pk', 'name', 'description', 'icon']
     search_fields = ['name', 'description']
     prepopulated_fields = {'slug': ('name',)}
-    inlines = [SpecialityPhotoInline, SymptomInline]
-
-
-@admin.register(Profession)
-class ProfessionAdmin(admin.ModelAdmin): 
-    list_display = ['name', 'speciality']
-    search_fields = ['name']
+    inlines = [SpecialityPhotoInline, SymptomInline, ProfessionInline]
