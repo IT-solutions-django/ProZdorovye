81ca6c90878ea00d65f58a93375d5a436ab0d947
Пак Сергей <sergegnabri2005@gmail.com>
2024-10-24 14:48:36 +0700

    Add password to Redis container

--- ProZdorovye/settings.py
+++ ProZdorovye/settings.py
@@ -159,16 +159,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Redis 
 REDIS_HOST = 'redis' 
-REDIS_PORT = '6379' 
 
 # Для локальной разработки
 # REDIS_HOST = 'localhost' 
-# REDIS_PORT = '6379' 
+
+REDIS_PORT = '6379' 
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
 
 # Celery
-CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0' 
+CELERY_BROKER_URL = f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0' 
 CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600} 
-CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0' 
+CELERY_RESULT_BACKEND = f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0' 
 CELERY_ACCEPT_CONTENT = ['application/json'] 
 CELERY_TASK_SERIALIZER = 'json' 
 CELERY_RESULT_serializer = 'json' 
