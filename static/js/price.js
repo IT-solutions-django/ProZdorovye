48813482fa8c9c834c6e259213f3a191f08bc299
Пак Сергей <sergegnabri2005@gmail.com>
2024-10-29 17:39:09 +0700

    Make ServiceType.duration nullable also on the frontend

--- static/js/price.js
+++ static/js/price.js
@@ -4,7 +4,7 @@ let directions=[],services=[];async function fetchDirections(){try{let e=await f
   </div>
 `;e.innerHTML+=t}),document.querySelectorAll(".directions__item").forEach(e=>{e.addEventListener("click",e=>{let i=e.target.innerText.trim();document.querySelector(".directions__item.active").classList.remove("active"),e.target.classList.add("active");let r=filterServicesByDirection(i);renderServices(r)})})}function filterServicesByDirection(e){return"Все направления"===e?services:services.filter(i=>i.direction===e)}function renderServices(e){let i=document.querySelector(".services");i.innerHTML="",e.forEach(e=>{let r=`
   <div class="services__item" data-id="${e.id}">
-    <div class="services__name">${e.name}, ${e.doctor},   ${e.duration} мин</div>
+    <div class="services__name">${e.name}, ${e.doctor}${e.duration !== null ? `, ${e.duration} мин` : ''}</div>
     <div class="services__price">${e.price} ₽</div>
     <a href="https://booking.medflex.ru/?user=5c4da05a2aec068f47734a86101c9333" target="_blank" class="services__button" target="_blank">Записаться на прием</a>
   </div>
