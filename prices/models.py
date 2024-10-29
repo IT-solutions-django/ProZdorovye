69dceab46b0bf2c2306d5c6dc24302cb9c3c8728
Пак Сергей <sergegnabri2005@gmail.com>
2024-10-29 17:20:54 +0700

    Make ServiceType.duration a nullable field

--- prices/models.py
+++ prices/models.py
@@ -8,7 +8,7 @@ class ServiceType(models.Model):
     price = models.DecimalField('Цена', decimal_places=2, max_digits=8)
     speciality = models.ForeignKey(verbose_name='Специализация', to=Speciality, on_delete=models.CASCADE, related_name='services')
     doctor = models.ForeignKey(verbose_name='Врач', to=Doctor, related_name='services', on_delete=models.CASCADE, null=True)
-    duration = models.SmallIntegerField(verbose_name='Длительность', default=60)
+    duration = models.SmallIntegerField(verbose_name='Длительность', default=None, null=True, blank=True)
 
     class Meta: 
         verbose_name = 'Тип услуги'
