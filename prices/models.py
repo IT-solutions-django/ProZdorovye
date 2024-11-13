596c86aa041bca8a6a6e14846e6484a1b6dced49
Пак Сергей <sergegnabri2005@gmail.com>
2024-11-13 10:51:24 +0700

    Add model for PDF document with prices

--- prices/models.py
+++ prices/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import os
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 from services.models import Speciality
 from doctors.models import Doctor
 
@@ -17,3 +20,46 @@ class ServiceType(models.Model):
     
     def __str__(self) -> str: 
         return f'{self.speciality} | {self.name} {self.price}р'
+    
+
+class PricePDF(models.Model): 
+    file = models.FileField('Прайс-лист', upload_to='prices')
+
+    class Meta: 
+        verbose_name = 'Прайс-лист'
+        verbose_name_plural = 'Прайс-лист'
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old_instance = PricePDF.objects.get(pk=self.pk)
+            if old_instance.file and old_instance.file != self.file:
+                if os.path.isfile(old_instance.file.path):
+                    old_instance.file.delete(save=False)
+        else: 
+            old_instance = PricePDF.objects.first() 
+            if old_instance and old_instance.file != self.file: 
+                if os.path.isfile(old_instance.file.path):
+                    old_instance.file.delete(save=False)
+
+        if PricePDF.objects.count():
+            self.pk = PricePDF.objects.first().pk
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.file:
+            self.file.delete(save=False)
+        super().delete(*args, **kwargs)
+
+    @classmethod
+    def get_instance(cls) -> "PricePDF":
+        instance = cls.objects.first()
+        return instance
+    
+    def __str__(self) -> str: 
+        return 'Прайс-лист'
+    
+
+@receiver(post_delete, sender=PricePDF)
+def delete_file_on_object_delete(sender, instance, **kwargs):
+    if instance.file:
+        instance.file.delete(save=False)
