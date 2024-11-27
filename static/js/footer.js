94910504152773c878558af5f232b2f466f47d0e
Пак Сергей <sergegnabri2005@gmail.com>
2024-11-27 12:21:26 +0700

    Change the copyright label from domain name of the site to name of company

--- static/js/footer.js
+++ static/js/footer.js
@@ -31,7 +31,7 @@ var footer=`    <div class="section section--footer">
                 Записаться на прием
               </button>
             </a>
-            <div class="footer__copyright">\xa9 2024 prozdorovie.site</div>
+            <div class="footer__copyright">\xa9 2024 ProЗдоровье</div>
           </div>
           <div class="social-links">
             <a href="https://t.me/pro_zdorovye_vl" target="_blank">
